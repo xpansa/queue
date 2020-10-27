6c0e1ffc98c64ee8916bc47edf5e952c26bdaa8a
Viktor Anikeenko <viktor@ventor.tech>
2020-10-27 17:00:56 +0300

    Backported https://github.com/OCA/connector/pull/270 to reduce number of created sessions

--- queue_job/jobrunner/runner.py
+++ queue_job/jobrunner/runner.py
@@ -139,6 +139,9 @@ ERROR_RECOVERY_DELAY = 5
 _logger = logging.getLogger(__name__)
 
 
+session = requests.Session()
+
+
 # Unfortunately, it is not possible to extend the Odoo
 # server command line arguments, so we resort to environment variables
 # to configure the runner (channels mostly).
@@ -198,6 +201,21 @@ def _odoo_now():
 
 
 def _async_http_get(port, db_name, job_uuid):
+    scheme = "http"
+    host = "localhost"
+    user = None
+    password = None
+
+    if not session.cookies:
+        # obtain an anonymous session
+        _logger.info("obtaining an anonymous session for the job runner")
+        url = ('%s://%s:%s/queue_job/session' % (scheme, host, port))
+        auth = None
+        if user:
+            auth = (user, password)
+        response = session.get(url, timeout=30, auth=auth)
+        response.raise_for_status()
+
     # Method to set failed job (due to timeout, etc) as pending,
     # to avoid keeping it as enqueued.
     def set_job_pending():
@@ -218,9 +236,12 @@ def _async_http_get(port, db_name, job_uuid):
         url = ('http://localhost:%s/queue_job/runjob?db=%s&job_uuid=%s' %
                (port, db_name, job_uuid))
         try:
+            auth = None
+            if user:
+                auth = (user, password)
             # we are not interested in the result, so we set a short timeout
             # but not too short so we trap and log hard configuration errors
-            response = requests.get(url, timeout=1)
+            response = session.get(url, timeout=1, auth=auth)
 
             # raise_for_status will result in either nothing, a Client Error
             # for HTTP Response codes between 400 and 500 or a Server Error
@@ -230,6 +251,7 @@ def _async_http_get(port, db_name, job_uuid):
             set_job_pending()
         except:
             _logger.exception("exception in GET %s", url)
+            session.cookies.clear()
             set_job_pending()
     thread = threading.Thread(target=urlopen)
     thread.daemon = True
